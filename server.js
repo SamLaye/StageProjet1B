c21538f4b7194c2deb454d5287086b6561a67cdc
Sam Laye <layesamss97@gmail.com>
2024-01-26 09:51:28 +0000

    Change case app to APP on server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const app = express()
+const APP = express()
 const cors = require('cors')
 const authRoutes = require('./routes/authRoutes')
 const hotelRoutes = require('./routes/hotelRoutes')
@@ -8,18 +8,18 @@ const PORT = process.env.PORT || 5000
 
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.json())
+APP.use(cors())
+APP.use(express.json())
 
 
-app.use("/api", authRoutes)
-app.use("/api/hotel", hotelRoutes)
+APP.use("/api", authRoutes)
+APP.use("/api/hotel", hotelRoutes)
 
 mongoose.connect(process.env.DB_URL).then((result)=>{
     console.log("Connected to DB, successfuly")
 }).catch(err => console.log(err))
 
-app.listen(PORT, () => {
+APP.listen(PORT, () => {
     console.log('Server started at port', PORT)
 })
 
