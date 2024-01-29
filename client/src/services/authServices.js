ff5ed2b0d6781d6a48553d26017d9cfc335e92a8
Sam Laye <layesamss97@gmail.com>
2024-01-29 08:56:50 +0000

    Failed Add Image - Changement de lien dans l'axios

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 // Axios est une bibliothèque JavaScript populaire qui permet d'effectuer des requêtes HTTP depuis un navigateur ou depuis Node.js. Elle simplifie l'interaction avec les API HTTP en fournissant une interface simple et puissante pour effectuer des requêtes.
 
-const SERVER_URL = 'http://localhost:5000/api'
-// const SERVER_URL = 'https://stageprojet1b-samlaye0.onrender.com/api'
+// const SERVER_URL = 'http://localhost:5000/api'
+const SERVER_URL = 'https://stageprojet1b-samlaye0.onrender.com/api'
 
 const registerUser = (data) => {
     return axios.post(SERVER_URL + '/register', data)
--- client/src/services/hotelServices.js
+++ client/src/services/hotelServices.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 // Axios est une bibliothèque JavaScript populaire qui permet d'effectuer des requêtes HTTP depuis un navigateur ou depuis Node.js. Elle simplifie l'interaction avec les API HTTP en fournissant une interface simple et puissante pour effectuer des requêtes.
 
-const SERVER_URL = 'http://localhost:5000/api/hotel'
+// const SERVER_URL = 'http://localhost:5000/api/hotel'
 // const SERVER_URL = 'https://stageprojet1b-samlaye.onrender.com/api'
-// const SERVER_URL = 'https://stageprojet1b-samlaye0.onrender.com/api'
+const SERVER_URL = 'https://stageprojet1b-samlaye0.onrender.com/api'
 
 const createHotel = (data) => {
     return axios.post(SERVER_URL + '/create-hotel', data)
